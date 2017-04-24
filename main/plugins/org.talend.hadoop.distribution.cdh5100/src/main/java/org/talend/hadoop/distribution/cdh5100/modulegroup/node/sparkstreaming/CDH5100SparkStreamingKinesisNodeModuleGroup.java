abf5bfea7d5ca5ec7c7ac4762678d09b0b11d8b0
BOURGEOIS Laurent <lbourgeois@talend.com>
2017-04-24 17:11:29 +0200

    TBD-4624 Add kinesis lib for sprk 2.1

--- main/plugins/org.talend.hadoop.distribution.cdh5100/src/main/java/org/talend/hadoop/distribution/cdh5100/modulegroup/node/sparkstreaming/CDH5100SparkStreamingKinesisNodeModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution.cdh5100/src/main/java/org/talend/hadoop/distribution/cdh5100/modulegroup/node/sparkstreaming/CDH5100SparkStreamingKinesisNodeModuleGroup.java
@@ -16,17 +16,40 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.ESparkVersion;
 import org.talend.hadoop.distribution.cdh5100.CDH5100Constant;
+import org.talend.hadoop.distribution.condition.BooleanOperator;
+import org.talend.hadoop.distribution.condition.ComponentCondition;
+import org.talend.hadoop.distribution.condition.EqualityOperator;
+import org.talend.hadoop.distribution.condition.LinkedNodeExpression;
+import org.talend.hadoop.distribution.condition.MultiComponentCondition;
+import org.talend.hadoop.distribution.condition.SimpleComponentCondition;
 import org.talend.hadoop.distribution.condition.common.SparkStreamingLinkedNodeCondition;
+import org.talend.hadoop.distribution.constants.SparkStreamingConstant;
 
 public class CDH5100SparkStreamingKinesisNodeModuleGroup {
 
+    private final static ComponentCondition spark16Condition = new SimpleComponentCondition(new LinkedNodeExpression(
+            SparkStreamingConstant.KAFKA_SPARKCONFIGURATION_LINKEDPARAMETER, "SUPPORTED_SPARK_VERSION", EqualityOperator.EQ,
+            ESparkVersion.SPARK_1_6.getSparkVersion()));
+
+    private final static ComponentCondition spark21Condition = new SimpleComponentCondition(new LinkedNodeExpression(
+            SparkStreamingConstant.KAFKA_SPARKCONFIGURATION_LINKEDPARAMETER, "SUPPORTED_SPARK_VERSION", EqualityOperator.EQ,
+            ESparkVersion.SPARK_2_1.getSparkVersion()));
+
     public static Set<DistributionModuleGroup> getModuleGroups(String distribution, String version) {
         Set<DistributionModuleGroup> hs = new HashSet<>();
-        DistributionModuleGroup dmg = new DistributionModuleGroup(
-                CDH5100Constant.SPARK_KINESIS_MRREQUIRED_MODULE_GROUP.getModuleName(), true,
-                new SparkStreamingLinkedNodeCondition(distribution, version).getCondition());
-        hs.add(dmg);
+        DistributionModuleGroup dmgSpark16 = new DistributionModuleGroup(
+                CDH5100Constant.SPARK_KINESIS_MRREQUIRED_MODULE_GROUP.getModuleName(), true, new MultiComponentCondition(
+                        new SparkStreamingLinkedNodeCondition(distribution, version).getCondition(), BooleanOperator.AND,
+                        spark16Condition));
+        
+        DistributionModuleGroup dmgSpark21 = new DistributionModuleGroup(
+                CDH5100Constant.SPARK2_KINESIS_MRREQUIRED_MODULE_GROUP.getModuleName(), true, new MultiComponentCondition(
+                        new SparkStreamingLinkedNodeCondition(distribution, version).getCondition(), BooleanOperator.AND,
+                        spark16Condition));
+        hs.add(dmgSpark16);
+        hs.add(dmgSpark21);
         return hs;
     }
 }
