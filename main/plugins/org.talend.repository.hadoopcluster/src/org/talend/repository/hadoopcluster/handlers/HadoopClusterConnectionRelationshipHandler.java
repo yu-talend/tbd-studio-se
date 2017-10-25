763472679f917b45a6e4a81994c0286b508d8bac
Chao MENG <cmeng@talend.com>
2017-10-25 19:20:20 +0800

    feat(TUP-18316): find a way to lower the cost of adding/upgrading BD Distros: Wizard (Import conf / build conf)

--- main/plugins/org.talend.repository.hadoopcluster/src/org/talend/repository/hadoopcluster/handlers/HadoopClusterConnectionRelationshipHandler.java
+++ main/plugins/org.talend.repository.hadoopcluster/src/org/talend/repository/hadoopcluster/handlers/HadoopClusterConnectionRelationshipHandler.java
@@ -19,11 +19,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
+import org.talend.core.model.metadata.builder.connection.Connection;
+import org.talend.core.model.metadata.designerproperties.RepositoryToComponentProperty;
+import org.talend.core.model.properties.DatabaseConnectionItem;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.relationship.AbstractItemRelationshipHandler;
 import org.talend.core.model.relationship.Relation;
 import org.talend.core.model.relationship.RelationshipItemBuilder;
 import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.designer.core.model.components.EParameterName;
 import org.talend.hadoop.distribution.dynamic.DynamicDistributionManager;
 import org.talend.repository.model.hadoopcluster.HadoopClusterConnection;
 import org.talend.repository.model.hadoopcluster.HadoopClusterConnectionItem;
@@ -38,6 +42,8 @@ public class HadoopClusterConnectionRelationshipHandler extends AbstractItemRela
     public boolean valid(Item baseItem) {
         if (baseItem instanceof HadoopClusterConnectionItem) {
             return true;
+        } else if (baseItem instanceof DatabaseConnectionItem) {
+            return true;
         } else {
             return false;
         }
@@ -47,6 +53,8 @@ public class HadoopClusterConnectionRelationshipHandler extends AbstractItemRela
     public String getBaseItemType(Item baseItem) {
         if (baseItem instanceof HadoopClusterConnectionItem) {
             return RelationshipItemBuilder.HADOOP_CLUSTER_RELATION;
+        } else if (baseItem instanceof DatabaseConnectionItem) {
+            return RelationshipItemBuilder.DB_CONNECTION_RELATION;
         } else {
             return null;
         }
@@ -73,6 +81,24 @@ public class HadoopClusterConnectionRelationshipHandler extends AbstractItemRela
                 }
             }
         }
+        if (baseItem instanceof DatabaseConnectionItem) {
+            Connection connection = ((DatabaseConnectionItem) baseItem).getConnection();
+            if (connection != null) {
+                Object value = RepositoryToComponentProperty.getValue(connection, EParameterName.DB_VERSION.getName(), null);
+                if (value instanceof String) {
+                    String version = (String) value;
+                    if (DynamicDistributionManager.getInstance().isUsersDynamicDistribution(version)) {
+                        Set<Relation> relationSet = new HashSet<Relation>();
+                        Relation addedRelation = new Relation();
+                        addedRelation.setId(version);
+                        addedRelation.setType(RelationshipItemBuilder.DYNAMIC_DISTRIBUTION_RELATION);
+                        addedRelation.setVersion(RelationshipItemBuilder.LATEST_VERSION);
+                        relationSet.add(addedRelation);
+                        return relationSet;
+                    }
+                }
+            }
+        }
         return Collections.emptySet();
     }
 
@@ -83,6 +109,11 @@ public class HadoopClusterConnectionRelationshipHandler extends AbstractItemRela
             ERepositoryObjectType hadoopClusterRepType = ERepositoryObjectType.valueOf("HADOOPCLUSTER"); //$NON-NLS-1$
             if (hadoopClusterRepType != null) {
                 repoTypes.add(hadoopClusterRepType);
+            }
+        }
+        if (RelationshipItemBuilder.DB_CONNECTION_RELATION.equals(relationType)) {
+            if (ERepositoryObjectType.METADATA_CONNECTIONS != null) {
+                repoTypes.add(ERepositoryObjectType.METADATA_CONNECTIONS);
             }
         }
         return repoTypes;
